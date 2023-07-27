3c20e8a919abc3c3a39866c90fa94505969a18b3
Jaz Baliola <baliola05@gmail.com>
2023-07-27 12:24:56 -0600

    update

--- user.js
+++ user.js
@@ -147,7 +147,7 @@ router.get('/new', (req, res) => {
         .then(([rows, fields]) => {
             // check if query affected a row
             if(rows.affectedRows > 0) {
-                res.redirect(callback+"#userAddedTrue");
+                window.open(callback+'#userAddedTrue', '_self');  
             } else {
                 res.redirect(callback+"#userAddedFalse");
             }
